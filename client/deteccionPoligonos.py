bc15457cb9e1d3d70d8032009d16cd25cfa4cc46
soydavidmarin <luis.1701624364@ucaldas.edu.co>
2022-05-26 15:04:01 -0500

    Reconocimiento y recorte de cuadrado

--- client/deteccionPoligonos.py
+++ client/deteccionPoligonos.py
@@ -7,6 +7,7 @@ Created on Wed Apr  6 10:59:13 2022
 
 import cv2
 import numpy as np
+from Cut import Cut
 
 nameWindow="Calculadora"
 def nothing(x):
@@ -16,7 +17,7 @@ def constructorVentana():
     cv2.createTrackbar("min",nameWindow,0,255,nothing)
     cv2.createTrackbar("max", nameWindow, 100, 255, nothing)
     cv2.createTrackbar("kernel", nameWindow, 1, 100, nothing)
-    cv2.createTrackbar("areaMin", nameWindow, 500, 10000, nothing)
+    cv2.createTrackbar("areaMin", nameWindow, 500, 1000, nothing)
     
 def calcularAreas(figuras):
     areas=[]
@@ -29,11 +30,15 @@ def detectarForma(imagen):
     # cv2.imshow("Gris", imagenGris)
     # cv2.imshow("HSV", imagenHSV)
     
-    min=cv2.getTrackbarPos("min", nameWindow)
-    max=cv2.getTrackbarPos("max", nameWindow)
+    # min=cv2.getTrackbarPos("min", nameWindow)
+    # max=cv2.getTrackbarPos("max", nameWindow)
+    min=196
+    max=255
+    
     bordes=cv2.Canny(imagenGris,min,max)
     
     tamañoKernel=cv2.getTrackbarPos("kernel", nameWindow)
+    tamañoKernel=10
     kernel=np.ones((tamañoKernel,tamañoKernel),np.uint8)
     bordes=cv2.dilate(bordes,kernel)
     cv2.imshow("Bordes",bordes)
@@ -42,40 +47,45 @@ def detectarForma(imagen):
     figuras,jerarquia=cv2.findContours(bordes,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     areas=calcularAreas(figuras)
     areaMin=cv2.getTrackbarPos("areaMin", nameWindow)
+    areaMin=0
     i=0
+    cut = Cut()    
     for figuraActual in figuras:
         if areas[i]>=areaMin:
             i=i+1
-            vertices=cv2.approxPolyDP(figuraActual,0.05*cv2.arcLength(figuraActual,True),True)
-            if len(vertices) == 3:
-                mensaje="Triangulo"
-                cv2.putText(imagen, mensaje, (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                cv2.drawContours(imagen, [figuraActual], 0, (0, 0, 255), 2)
+            vertices=cv2.approxPolyDP(figuraActual,0.05*cv2.arcLength(figuraActual,True),True)            
             if len(vertices) == 4:
                 mensaje="Cuadrado"
                 cv2.putText(imagen, mensaje, (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
                 cv2.drawContours(imagen, [figuraActual], 0, (0, 0, 255), 2)
-            if len(vertices) == 5:
-                mensaje="Pentagono"
-                cv2.putText(imagen, mensaje, (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                cv2.drawContours(imagen, [figuraActual], 0, (0, 0, 255), 2)
+                cut.crop(imagenGris, [figuraActual], 1)
+                
+
     return imagen
 
-def mostrarMensaje(mensaje, imagen,figuraActual):
-    cv2.putText(imagen, mensaje, (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.drawContours(imagen, [figuraActual], 0, (0, 0, 255), 2)
+# def mostrarMensaje(mensaje, imagen,figuraActual):
+#     cv2.putText(imagen, mensaje, (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+#     # cv2.drawContours(imagen, [figuraActual], 0, (0, 0, 255), 2)
     
 #Apertura cámara
+# cut = Cut()
 video=cv2.VideoCapture(0)
 constructorVentana()
 bandera=True
+mostrar=False
 while bandera:
-    _,imagen = video.read()
-    imagen=detectarForma(imagen)
-    cv2.imshow("Imagen", imagen)
+    _,imagen = video.read()     
+    
     #Parar el programa
     k = cv2.waitKey(5) & 0xFF
+    if k == 99:
+        mostrar = True     
     if k == 27:
         bandera=False
+    if mostrar:
+        imagen=detectarForma(imagen)  
+        cv2.imshow("Imagen", imagen)
+        # cut.crop()
+    cv2.imshow("Imagen", imagen)
 video.release()
 cv2.destroyAllWindows()
